e9c0e9b354df00fe384fb84f8c718e9601302a2d
Joseph Maher <joseph.maher@csi.cuny.edu>
2023-08-15 17:55:02 -0400

    use julia commands to install IJulia kernel

--- julia_install.sh
+++ julia_install.sh
@@ -56,9 +56,9 @@ touch $julia_global_env/Project.toml
 # --> see below
 
 # Install IJulia
-julia --project=$julia_global_env -e 'deleteat!(DEPOT_PATH, [1,3]); using Pkg; Pkg.update(); Pkg.add("IJulia"); Pkg.precompile()'
+julia --project=$julia_global_env -e 'deleteat!(DEPOT_PATH, [1,3]); using Pkg; Pkg.update(); Pkg.add("IJulia"); Pkg.precompile(); using IJulia; installkernel("Julia")'
 # and make the kernel available to TLJH
-cp -r ~/.local/share/jupyter/kernels/julia-$julia_version_short /opt/tljh/user/share/jupyter/kernels/
+#cp -r ~/.local/share/jupyter/kernels/julia-$julia_version_short /opt/tljh/user/share/jupyter/kernels/
 
 # Install Plots
 julia --project=$julia_global_env -e 'deleteat!(DEPOT_PATH, [1,3]); using Pkg; Pkg.update(); Pkg.add("Plots"); Pkg.add("PlotlyBase"); Pkg.precompile()'
