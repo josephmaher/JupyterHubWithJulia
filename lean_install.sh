42e45117b0c5c5588c4a0b5b6e3c2847bcabcfe9
Joseph Maher <joseph.maher@csi.cuny.edu>
2025-09-04 21:02:46 -0400

    Change from openvpn to headscale / tailscale

--- lean_install.sh
+++ lean_install.sh
@@ -20,6 +20,30 @@ source $HOME/.elan/env
 
 # now install lean4_jupyter
 
-git clone https://github.com/utensil/lean4_jupyter.git && cd lean4_jupyter && ./scripts/prep.sh
+# automatic istallation puts things in the wrong place
+# git clone https://github.com/utensil/lean4_jupyter.git && cd lean4_jupyter && ./scripts/prep.sh
 
+# need to do manual installation to use previously installed jupyterhub
 
+lean --version
+
+git clone https://github.com/utensil/lean4_jupyter.git
+
+git clone https://github.com/leanprover-community/repl
+
+cd repl
+
+lake build
+
+ln -s /opt/lean/repl/.lake/build/bin/repl $HOME/.elan/bin/repl
+
+HOME=/root
+
+# make sure we have the previously installed pip in the path
+export PATH=/opt/tljh/user/bin:${PATH}
+
+pip install lean4_jupyter
+
+python -m lean4_jupyter.install
+
+# cp -a /usr/local/share/jupyter/kernels/lean4 /opt/tljh/user/share/jupyter/kernels
